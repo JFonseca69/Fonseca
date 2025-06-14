168e513d67efcabd4b19331538e00c901e520b77
JFonseca69 <joaofonseca.assessoria@gmail.com>
2025-06-13 23:41:45 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+// Código simplificado do script.js conforme fornecido anteriormente
+// O conteúdo completo será detalhado ou expandido sob demanda
+console.log("Script carregado - insira o código JS completo aqui...");
